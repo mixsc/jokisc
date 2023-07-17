4f6fee01f2dd9021c5cdc25f770d47bcf2201383
mixsc <139757356+mixsc@users.noreply.github.com>
2023-07-18 03:25:01 +0700

    Add files via upload

---
+++ xp-deobfuscated.sh
@@ -0,0 +1,159 @@
+#Deobfuscated By xNot_Found
+#Github : https://github.com/hatakecnk
+BIBlack='[1;90m'      # Black
+BIRed='[1;91m'        # Red
+BIGreen='[1;92m'      # Green
+BIYellow='[1;93m'     # Yellow
+BIBlue='[1;94m'       # Blue
+BIPurple='[1;95m'     # Purple
+BICyan='[1;96m'       # Cyan
+BIWhite='[1;97m'      # White
+UWhite='[4;37m'       # White
+On_IPurple='[0;105m'  #
+On_IRed='[0;101m'
+IBlack='[0;90m'       # Black
+IRed='[0;91m'         # Red
+IGreen='[0;92m'       # Green
+IYellow='[0;93m'      # Yellow
+IBlue='[0;94m'        # Blue
+IPurple='[0;95m'      # Purple
+ICyan='[0;96m'        # Cyan
+IWhite='[0;97m'       # White
+NC='\e[0m'
+export RED='[0;31m'
+export GREEN='[0;32m'
+export YELLOW='[0;33m'
+export BLUE='[0;34m'
+export PURPLE='[0;35m'
+export CYAN='[0;36m'
+export LIGHT='[0;37m'
+export NC='[0m'
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+BURIQ () {
+curl -sS https://raw.githubusercontent.com/mixsc/permission/main/ip > /root/tmp
+data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+for user in "${data[@]}"
+do
+exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+d1=(`date -d "$exp" +%s`)
+d2=(`date -d "$biji" +%s`)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+echo $user > /etc/.$user.ini
+else
+rm -f /etc/.$user.ini > /dev/null 2>&1
+fi
+done
+rm -f /root/tmp
+}
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/mixsc/permission/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+if [ "$CekOne" = "$CekTwo" ]; then
+res="Expired"
+fi
+else
+res="Permission Accepted..."
+fi
+}
+PERMISSION () {
+MYIP=$(curl -sS ipv4.icanhazip.com)
+IZIN=$(curl -sS https://raw.githubusercontent.com/mixsc/permission/main/ip | awk '{print $4}' | grep $MYIP)
+if [ "$MYIP" = "$IZIN" ]; then
+Bloman
+else
+res="Permission Denied!"
+fi
+BURIQ
+}
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
+fi
+done
+data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
+fi
+done
+data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
+fi
+done
+systemctl restart xray
+hariini=`date +%d-%m-%Y`
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+do
+tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+username=`echo $tuserval | cut -f1 -d:`
+userexp=`echo $tuserval | cut -f2 -d:`
+userexpireinseconds=$(( $userexp * 86400 ))
+tglexp=`date -d @$userexpireinseconds`
+tgl=`echo $tglexp |awk -F" " '{print $3}'`
+while [ ${#tgl} -lt 2 ]
+do
+tgl="0"$tgl
+done
+while [ ${#username} -lt 15 ]
+do
+username=$username" "
+done
+bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+todaystime=`date +%s`
+if [ $userexpireinseconds -ge $todaystime ] ;
+then
+:
+else
+userdel --force $username
+fi
+done
