bec4c74614fdde25cd71fbff56c265e73994bfad
mixsc <139757356+mixsc@users.noreply.github.com>
2023-07-28 16:03:05 +0700

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -126,9 +126,9 @@ clear
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#trojanws$/a\### '"$user $exp"'\
+sed -i '/#trojanws$/a\#! '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#trojangrpc$/a\### '"$user $exp"'\
+sed -i '/#trojangrpc$/a\#! '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 
 systemctl restart xray
